75aa1f11b28e2aca5b054adc49f1dd5e4fe2f3c9
reuelethan <reuelbeats@gmail.com>
2019-11-03 17:43:24 -0500

    submits hard coded number

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-document.querySelector(".weeklyBudget").innerHTML = `
-<div class="weeklybudget"> Number Goes Here
-</div> `;
+// document.querySelector(".weeklyBudget").innerHTML = `
+// <div class="weeklybudget"> Number Goes Here
+// </div> `;
 
 
-//test 2//
-let earned = (document.querySelector("#earnings"));
-document.querySelector("#earned").addEventListener("submit", function budgetValue(e) {
+// //test 2//
+// let earned = (document.querySelector("#earnings"));
+// document.querySelector("#earned").addEventListener("submit", function budgetValue(e) {
 
-    document.querySelector(".weeklyBudget").innerHTML = (`$${earned.value}`);
-    e.preventDefault();
+//     e.preventDefault();
+//     document.querySelector(".weeklyBudget").innerHTML = (`$${earned.value}`);
 
-})
+// })
 
 //Test 3
-class UI {
-    constructor(earned) {
-        this.earned = document.querySelector(earned.value);
-        console.log(this.earned);
-        this.expenseFeedback = document.querySelector(".expense-feedback");
-        this.budgetForm = document.getElementById("budget-form");
-        this.budgetInput = document.getElementById("budget-input");
-        this.budgetAmount = document.getElementById("budget-amount");
-        this.expenseAmount = document.getElementById("expense-amount");
-        this.balance = document.getElementById("balance");
-        this.balanceAmount = document.getElementById("balance-amount");
-        this.expenseForm = document.getElementById("expense-form");
-        this.expenseInput = document.getElementById("expense-input");
-        this.amountInput = document.getElementById("amount-input");
-        this.expenseList = document.getElementById("expense-list");
-        this.itemList = [];
-        this.itemID = 0;
-    }
-    new UI {
-        consturctor(earned)
-        console.log(earned)
-    }
+// class UI {
+//     constructor(earned) {
+//         this.earned = document.querySelector(earned.value);
+//         console.log(this.earned);
+//         this.expenseFeedback = document.querySelector(".expense-feedback");
+//         this.budgetForm = document.getElementById("budget-form");
+//         this.budgetInput = document.getElementById("budget-input");
+//         this.budgetAmount = document.getElementById("budget-amount");
+//         this.expenseAmount = document.getElementById("expense-amount");
+//         this.balance = document.getElementById("balance");
+//         this.balanceAmount = document.getElementById("balance-amount");
+//         this.expenseForm = document.getElementById("expense-form");
+//         this.expenseInput = document.getElementById("expense-input");
+//         this.amountInput = document.getElementById("amount-input");
+//         this.expenseList = document.getElementById("expense-list");
+//         this.itemList = [];
+//         this.itemID = 0;
+//     }
+//     new UI {
+//         consturctor(earned)
+//         console.log(earned)
+//     }
+// }
+function budgetValue() {
+
+    // e.preventDefault();
+    document.querySelector(".weeklyBudget").innerHTML = (`777`);
+
 }
+
+let earned = (document.querySelector("#earnings"));
+document.querySelector("#earned").addEventListener("submit", budgetValue)
