d955be81bca3b8d0aa4b38e0ac196bc40f660604
reuelethan <reuelbeats@gmail.com>
2019-11-04 13:22:55 -0500

    final

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let userBudget = undefined;
+
 function budgetValue(e) {
-  e.preventDefault();
-  let zeroFix = document.querySelector("#earnings").valueAsNumber || "0";
-  document.querySelector(".weeklyBudget").innerText = "$" + zeroFix;
-  userBudget = document.querySelector(".weeklyBudget").innerText.slice(1);
-  console.log(userBudget);
-  document.querySelector("#earned").style.display = "none";
+    e.preventDefault();
+    let zeroFix = document.querySelector("#earnings").valueAsNumber || "0";
+    document.querySelector(".weeklyBudget").innerText = "$" + zeroFix;
+    userBudget = document.querySelector(".weeklyBudget").innerText.slice(1);
+    console.log(userBudget);
+    document.querySelector("#earned").style.display = "none";
 }
 
 let earned = document.querySelector("#earnings");
@@ -19,66 +20,66 @@ let expensesArray = [];
 let totalEnetertainment = (totalFood = totalClothing = totalBills = totalExpenses = totalSavings = 0);
 
 function expenseValue(e) {
-  e.preventDefault();
-  expenseName = document.querySelector(".Name").value;
-  expensePrice = document.querySelector(".Price").valueAsNumber;
-  expenseCategory =
-    dropDownContent.options[dropDownContent.selectedIndex].value;
-  console.log(`
+    e.preventDefault();
+    expenseName = document.querySelector(".Name").value;
+    expensePrice = document.querySelector(".Price").valueAsNumber;
+    expenseCategory =
+        dropDownContent.options[dropDownContent.selectedIndex].value;
+    console.log(`
     Expense Category: ${expenseCategory}.
     Expense Name: ${expenseName}.
     Expense Price: ${expensePrice}.`);
-  expensesArray.push([expenseCategory, expenseName, expensePrice]);
-  // totals cost of each item from each category
-  if (expenseCategory == "Entertainment") {
-    totalEnetertainment += expensePrice;
-  } else if (expenseCategory == "Food") {
-    totalFood += expensePrice;
-  } else if (expenseCategory == "Clothing") {
-    totalClothing += expensePrice;
-  } else if (expenseCategory == "Bills") {
-    totalBills += expensePrice;
-  }
-  totalExpenses = totalFood + totalBills + totalClothing + totalEnetertainment;
-  totalSavings = userBudget - totalExpenses;
-  console.log(`
+    expensesArray.push([expenseCategory, expenseName, expensePrice]);
+    // totals cost of each item from each category
+    if (expenseCategory == "Entertainment") {
+        totalEnetertainment += expensePrice;
+    } else if (expenseCategory == "Food") {
+        totalFood += expensePrice;
+    } else if (expenseCategory == "Clothing") {
+        totalClothing += expensePrice;
+    } else if (expenseCategory == "Bills") {
+        totalBills += expensePrice;
+    }
+    totalExpenses = totalFood + totalBills + totalClothing + totalEnetertainment;
+    totalSavings = userBudget - totalExpenses;
+    console.log(`
     budget: ${userBudget}.
     total expenses: ${totalExpenses}.
     savings left: ${totalSavings}. 
     `);
-  if (totalSavings <= 0) {
-    alert("Your budget is 0");
-  }
+    if (totalSavings <= 0) {
+        alert("Your budget is 0");
+    }
 
-  //for graphics
-  document.querySelector("#foodBar").style.width = `${(totalFood / userBudget) *
+    //for graphics
+    document.querySelector("#foodBar").style.width = `${(totalFood / userBudget) *
     100}%`;
-  document.querySelector("#billsBar").style.width = `${(totalBills /
+    document.querySelector("#billsBar").style.width = `${(totalBills /
     userBudget) *
     100}%`;
-  document.querySelector("#clothingBar").style.width = `${(totalClothing /
+    document.querySelector("#clothingBar").style.width = `${(totalClothing /
     userBudget) *
     100}%`;
-  document.querySelector(
-    "#entertainmentBar"
-  ).style.width = `${(totalEnetertainment / userBudget) * 100}%`;
-  document.querySelector("article").innerHTML += `
+    document.querySelector(
+        "#entertainmentBar"
+    ).style.width = `${(totalEnetertainment / userBudget) * 100}%`;
+    document.querySelector("article").innerHTML += `
     <p class="itemized">
-   <span> ${expenseCategory}:</span> <span>${expenseName} </span><span> ${expensePrice}</span>
+   <span class="listed"> ${expenseCategory}:</span> <span class="listed">${expenseName} </span><span class="listed"> $ ${expensePrice}</span>
     </p>
     
     
     
     `;
-  //animate
-  document.querySelectorAll("#graphics div").forEach(function(el) {
-    el.classList.add("animator");
-    setTimeout(function() {
-      el.classList.remove("animator");
-    }, 1001);
-  });
+    //animate
+    document.querySelectorAll("#graphics div").forEach(function (el) {
+        el.classList.add("animator");
+        setTimeout(function () {
+            el.classList.remove("animator");
+        }, 1001);
+    });
 }
 
 document
-  .querySelector("#expenses-button")
-  .addEventListener("click", expenseValue);
+    .querySelector("#expenses-button")
+    .addEventListener("click", expenseValue);
